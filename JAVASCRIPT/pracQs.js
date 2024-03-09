986cb4fe134382bbda7277e910d6a2f67898f6fd
Sarita Maurya <sm7767119@gmail.com>
2024-03-09 14:27:27 +0530

    JavaScript

---
+++ JAVASCRIPT/pracQs.js
@@ -0,0 +1,32 @@
+let age=23;
+age+2;//after 2 year
+console.log(age);
+
+// Next Question
+
+let hindi=80;
+let eng=90;
+let math=100;
+let avg=(hindi+eng+math)/3;
+console.log("the avarage is:"+ avg);
+// 
+console.log(270/3);
+
+// unary operator
+
+let Age=10;
+// let newAge=++Age;
+let newAge=Age++;
+console.log(newAge);
+console.log(Age);
+
+let num=5;
+let Newnum=num++;
+console.log(Newnum);
+console.log(num);
+Newnum=++num;
+console.log(num);
+console.log(Newnum);
+
+
+
