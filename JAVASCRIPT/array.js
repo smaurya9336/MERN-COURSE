30dca32868ad500615cd2e19aa775dff379017c8
Sarita Maurya <sm7767119@gmail.com>
2024-03-11 10:43:52 +0530

    Array

---
+++ JAVASCRIPT/array.js
@@ -0,0 +1,40 @@
+// Array
+
+const myArr=[0,1,4,8,5]
+// const myHeros=["Saktiman","Najraj"]
+// const myArr2=[30,20,3,8,6]
+// console.log(myArr[1]);
+// console.log(myArr2[2]);
+// console.log(myArr);
+// console.log(myHeros);
+
+//Array Methods
+
+// console.log(myArr.push(6));
+// console.log(myArr.pop());
+// myArr.push(7)
+// myArr.push(8)
+// myArr.pop()
+
+
+// myArr.unshift(9);
+// myArr.shift()
+// console.log(myArr);
+
+
+console.log("A ",myArr);
+const myn1=myArr.slice(1,3)
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2=myArr.splice(1,3)
+console.log(myn2);
+console.log("C ",myArr);
+
+
+
+
+
+
+
+
