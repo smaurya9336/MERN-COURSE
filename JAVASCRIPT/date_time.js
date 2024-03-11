3518df9a582f4c14862f98532b2c283d99a02928
Sarita Maurya <sm7767119@gmail.com>
2024-03-11 10:15:03 +0530

    date and time

---
+++ JAVASCRIPT/date_time.js
@@ -0,0 +1,36 @@
+// Dates
+
+let myDate=new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreateDate=new Date(2024,2,12,5,7)
+// console.log(myCreateDate.toDateString());
+
+// let myCreateDate=new Date("2024-01-11")
+let myCreateDate=new Date("01-11-2023")
+// console.log(myCreateDate.toLocaleString());
+let newDate=new Date()
+// console.log(newDate.getFullYear());
+// console.log(newDate.getMonth() +1);
+// console.log(newDate.getDay());
+
+
+// newDate.toLocaleString('default',{
+//     weekday: "long",
+//     minute: myCreateDate,
+// })
+
+
+
+
+
+
+
+
