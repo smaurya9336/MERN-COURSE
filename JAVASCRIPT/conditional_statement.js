307e0b1f69217df0f796c4e442ead038111549d4
Sarita Maurya <sm7767119@gmail.com>
2024-03-12 11:05:31 +0530

    If statement

---
+++ JAVASCRIPT/conditional_statement.js
@@ -0,0 +1,14 @@
+// let age=20;
+// if(age>=18){
+//     console.log("Valid Voter");
+   
+// }
+
+// else{
+//     console.log("Invalid voter");
+// }
+
+let firstName="Sarita"
+if(firstName=="Sarita"){
+    console.log("WELCOME! Sarita");
+}
