b82585ab2aa722db761a0d616c88f8ad6b73194e
Sarita Maurya <sm7767119@gmail.com>
2024-03-12 13:11:56 +0530

    Switch Case

---
+++ JAVASCRIPT/switch_Statement.js
@@ -0,0 +1,43 @@
+let color="white";
+switch(color){
+    case "red":
+        console.log("Stop.");
+        break;
+    case "yellow":
+        console.log("Slow down.");
+        break;
+    case "green":
+        console.log("Go.");
+        break;
+    default:
+        console.log("Broken Light");
+    
+}
+
+// print week days
+let day=3;
+switch(day){
+    case 1:
+        console.log("today is Monday.");
+        break;
+    case 2:
+        console.log("today is Tuesday.");
+        break;
+    case 3:
+        console.log("today is Wednesday.");
+        break;
+    case 4:
+        console.log("today is Thursday.");
+        break;
+    case 5:
+        console.log("today is Friday.");
+        break;
+    case 6:
+        console.log("today is Saturday.");
+        break;
+    case 7:
+        console.log("today is Sunday and fun day.");
+        break;
+    default:
+        console.log("Wrong day.");
+}
