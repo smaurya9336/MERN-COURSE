2ae4b9866d207241203e05d61359182986e26b00
Sarita Maurya <sm7767119@gmail.com>
2024-03-12 12:58:32 +0530

    Practice Question

--- JAVASCRIPT/app1.js
+++ JAVASCRIPT/app1.js
@@ -1,13 +1,39 @@
-let size="XL"
-if(size==="XL"){
-    console.log("Price is Rs. 250");
-}
-else if(size==="L"){
-    console.log("Price is Rs. 200");
-}
-else if(size==="M"){
-    console.log("Price is Rs. 100");
+// let size="XL"
+// if(size==="XL"){
+//     console.log("Price is Rs. 250");
+// }
+// else if(size==="L"){
+//     console.log("Price is Rs. 200");
+// }
+// else if(size==="M"){
+//     console.log("Price is Rs. 100");
+// }
+// else{
+//     console.log("Price is Rs. 50");
+// }
+
+
+// good string
+
+let str="Apple";
+if(str[0]==="A" && str.length>3){
+    console.log("Good string.");
 }
 else{
-    console.log("Price is Rs. 50");
+    console.log("not a good string.");
 }
+
+let num=12;
+if((num%2===0) && ((num+1==15) || (num-1==11))){
+    console.log("safe");
+}
+else{
+    console.log("unsafe");
+}
+
+
+
+
+
+
+
