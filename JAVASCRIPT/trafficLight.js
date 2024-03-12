cf9c443ede378dfc37a466fb9379dbae491bfeb1
Sarita Maurya <sm7767119@gmail.com>
2024-03-12 11:15:56 +0530

    Traffic Light

---
+++ JAVASCRIPT/trafficLight.js
@@ -0,0 +1,12 @@
+// Create a Traffic Light
+
+let color="yellow";
+if(color==="red"){
+    console.log("Stop. Light color is Red");
+}
+if(color==="yellow"){
+    console.log("Slow down. Light color is Yellow");
+}
+if(color==="green"){
+    console.log("Go. Light color is Green");
+}
