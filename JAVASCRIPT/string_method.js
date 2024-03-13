65de88d8af10c4909f300c2ff5dfd6dd63440360
Sarita Maurya <sm7767119@gmail.com>
2024-03-13 12:17:43 +0530

    ARRAY METHODS

--- JAVASCRIPT/string_method.js
+++ JAVASCRIPT/string_method.js
@@ -16,9 +16,27 @@
 
 //  string slice 
 
-let str="ILoveCoding";
-console.log(str.slice(1,5));
-console.log(str.slice(5));
-console.log(str.replace("Love","do"));
-console.log(str.repeat(2));
+// let str="ILoveCoding";
+// console.log(str.slice(1,5));
+// console.log(str.slice(5));
+// console.log(str.replace("Love","do"));
+// console.log(str.repeat(2));
 
+//Array
+
+let fruits=["banana","papaya","mango"]
+fruits[10]="pineapple"
+console.log(fruits[0]);
+console.log(fruits);
+
+// array method
+// push:add to value end
+// pop:remove value fro end
+// unshift:add value to 0 index 
+// shift:remove value from 0 index
+
+let months=["january","july","march","August"]
+console.log(months);
+months[0]="july";
+months[1]="june";
+console.log(months);
