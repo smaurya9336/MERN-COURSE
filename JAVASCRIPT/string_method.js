796d2a001a663f5668133995300c9fec3ae32297
Sarita Maurya <sm7767119@gmail.com>
2024-03-13 11:45:44 +0530

    Srting Methods

---
+++ JAVASCRIPT/string_method.js
@@ -0,0 +1,24 @@
+// let str="    Hello  ";
+// console.log(str);
+// console.log(str.trim());
+
+// let name="Sarita Maurya";
+// console.log(name.toUpperCase());
+// let msg="ILoveCoding"
+// console.log(msg.indexOf("Love"));
+// console.log(msg.indexOf("o"));
+
+// method chaining
+
+// let msg="      Hello   "
+// console.log(msg.trim().toUpperCase());
+
+
+//  string slice 
+
+let str="ILoveCoding";
+console.log(str.slice(1,5));
+console.log(str.slice(5));
+console.log(str.replace("Love","do"));
+console.log(str.repeat(2));
+
