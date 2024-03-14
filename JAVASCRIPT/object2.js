f7aefb9f864022c75aa87377278907f5547446c6
Sarita Maurya <sm7767119@gmail.com>
2024-03-14 14:27:57 +0530

    Destructuring of object

--- JAVASCRIPT/object2.js
+++ JAVASCRIPT/object2.js
@@ -48,11 +48,29 @@ const users=
 
 ]
 // users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
 
-console.log(Object.entries(tinderUser));
+// console.log(Object.entries(tinderUser));
 
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+//destructuring of objects
+
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+// course.courseInstructor
+
+
+const {courseInstructor:Instructor}=course
+console.log(Instructor);
 
