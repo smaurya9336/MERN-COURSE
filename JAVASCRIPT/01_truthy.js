53d8d62fed63581680ef690e00cf8622d56d3bc5
Sarita Maurya <sm7767119@gmail.com>
2024-03-15 12:06:39 +0530

    ternary operator

--- JAVASCRIPT/01_truthy.js
+++ JAVASCRIPT/01_truthy.js
@@ -23,10 +23,20 @@ if (Object.keys(emptyObj).length===0) {
 
 
 
-
+//null and undefined
 let val1;
 // val1=5 ?? 10;
-val1=null ?? 10;
+// val1=null ?? 10;
+
+val1 = undefined ?? 20;
 
 
 console.log(val1);
+
+
+//ternary operator
+
+// condition ? true :false
+const iceTeaPrice=100
+
+iceTeaPrice <= 80? console.log("less than 80"):console.log("more than 80");;
