067397793f69b48a3a3ffdb613be7901ff7c26bb
Sarita Maurya <sm7767119@gmail.com>
2024-03-21 13:33:09 +0530

    try and caught

--- JAVASCRIPT/01_thisKey.js
+++ JAVASCRIPT/01_thisKey.js
@@ -1,17 +1,32 @@
-const students={
-    name:"sarita",
-    age:23,
-    eng:95,
-    math:89,
-    phy:95,
-    getAvg(){
-        console.log(this);
-        let avg = (this.eng + this.math + this.phy) / 3;
-        console.log(`${avg}`);
-        // console.log(students.getAvg());
-    },
+// const students={
+//     name:"sarita",
+//     age:23,
+//     eng:95,
+//     math:89,
+//     phy:95,
+//     getAvg(){
+//         // console.log(this);
+//         let avg = (this.eng + this.math + this.phy) / 3;
+//         console.log(`${avg}`);
+//         // console.log(students.getAvg());
+//     },
 
      
+// }
+// students.getAvg();
+// console.log(students.getAvg());
+
+
+console.log("hello");
+console.log("hello");
+let a=10;
+try {
+    console.log(a);
+    
+} catch(error){
+    console.log("caught an error .. a is not defined");
+    console.log(error);
+    
 }
-students.getAvg();
-console.log(students.getAvg());
+console.log("hello world");
+console.log("hello world");
