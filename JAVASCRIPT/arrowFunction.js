070e8dc65baf935e49afb8a99952cc70d2a8a6bd
Sarita Maurya <sm7767119@gmail.com>
2024-03-21 13:46:06 +0530

    arrow function

---
+++ JAVASCRIPT/arrowFunction.js
@@ -0,0 +1,14 @@
+const sum =(a,b) =>{
+    console.log(a+b);
+
+}
+sum(10,20)
+
+const hello=n=>{
+    console.log(n*n*n);
+}
+hello(10)
+
+const multiply=(a,b)=>{console.log(a*b)};
+
+multiply(2,3)
