25e207ea8d787013833a275ae90874792005f7a5
Sarita Maurya <sm7767119@gmail.com>
2024-03-30 11:22:25 +0530

    spread

--- JAVASCRIPT/01_mapFunc.js
+++ JAVASCRIPT/01_mapFunc.js
@@ -7,3 +7,12 @@ let ans=number.filter((items)=>{
 let red=[1,2,3,4]
 let final=red.reduce((res,item)=>(res+item))
 console.log(final);
+
+
+console.log(..."Sarita");
+let odd=[1,3,5,7,9]
+let even=[2,4,6,8]
+
+let nums=[...odd,...even]
+
+console.log(nums);
