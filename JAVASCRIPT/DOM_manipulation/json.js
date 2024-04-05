7b0fa8cb237939cec1338731022c986bd6ad1e23
Sarita Maurya <sm7767119@gmail.com>
2024-04-05 12:06:15 +0530

    dog picture

--- JAVASCRIPT/DOM_manipulation/json.js
+++ JAVASCRIPT/DOM_manipulation/json.js
@@ -9,32 +9,71 @@
 // hoppscotch--testing api request tool
 // https://hoppscotch.io/
 
-
+let url2="https://dog.ceo/api/breeds/image/random";
 let btn=document.querySelector("button");
+
+
 btn.addEventListener("click",async ()=>{
-    let fact= await getFacts();
-    // console.log(fact);
-    let p=document.querySelector("#result");
-    p.innerText=fact;
+    // let fact= await getFacts();
+    // // console.log(fact);
+    // let p=document.querySelector("#result");
+    // p.innerText=fact;
 });
 
-
-
-let  url="https://catfact.ninja/fact";
-
-
-async function getFacts(){
+async function getImage(){
     try{
-        let res=await axios.get(url);
-        return res.data.fact;
+        let res=await axios.get(url2);
+        console.log(res.data.message);
     }
     catch(e){
         console.log("error",e);
-        return "NO FACT FOUND";
+        return "NO IMAGE FOUND";
     }
 }
 
-getFacts();
+getImage();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// btn.addEventListener("click",async ()=>{
+//     let fact= await getFacts();
+//     // console.log(fact);
+//     let p=document.querySelector("#result");
+//     p.innerText=fact;
+// });
+
+
+
+// let  url="https://catfact.ninja/fact";
+
+
+
+// async function getFacts(){
+//     try{
+//         let res=await axios.get(url);
+//         return res.data.fact;
+//     }
+//     catch(e){
+//         console.log("error",e);
+//         return "NO FACT FOUND";
+//     }
+// }
+
+// getFacts();
 
 // async function getFacts(){
     // try{
