b1c82ee48e789bee4109b448c5a4ff89e0ed65ce
Sarita Maurya <sm7767119@gmail.com>
2024-04-05 11:24:40 +0530

    first api request

--- JAVASCRIPT/DOM_manipulation/json.js
+++ JAVASCRIPT/DOM_manipulation/json.js
@@ -1,7 +1,39 @@
-let student = {
-    name:"sarita",
-    marks:89
-};
+// let student = {
+//     name:"sarita",
+//     marks:89
+// };
 
-let json=JSON.stringify(student);
-console.log(json);
+// let json=JSON.stringify(student);
+// console.log(json);
+
+// hoppscotch--testing api request tool
+// https://hoppscotch.io/
+
+let  url="https://catfact.ninja/fact";
+
+fetch(url)
+.then((response)=>{
+    console.log(response);
+     return response.json();
+})
+.then((data)=>{
+    console.log(data.fact);
+    return fetch(url)
+})
+.catch((err)=>{
+    console.log("ERROR",err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
