341514834628ee21484e09b63f90a4185051dc02
Sarita Maurya <sm7767119@gmail.com>
2024-04-05 11:32:12 +0530

    async await

--- JAVASCRIPT/DOM_manipulation/json.js
+++ JAVASCRIPT/DOM_manipulation/json.js
@@ -12,9 +12,19 @@
 let  url="https://catfact.ninja/fact";
 
 async function getFacts(){
+    try{
     let res= await fetch(url);
     let data=await res.json();
     console.log(data.fact);
+    
+    let res2= await fetch(url);
+    let data2=await res.json();
+    console.log(data2.fact);
+    }
+    catch(e){
+        console.log(e);
+    }
+    console.log("bye");
 }
 
 getFacts();
