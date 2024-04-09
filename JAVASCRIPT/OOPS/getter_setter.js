d30c54995c773350d24d63c7cd5b878ab1700a2c
Sarita Maurya <sm7767119@gmail.com>
2024-04-09 13:06:32 +0530

    getterandsetter

--- JAVASCRIPT/OOPS/getter_setter.js
+++ JAVASCRIPT/OOPS/getter_setter.js
@@ -1,16 +1,28 @@
+// getter and setter
+
 class User{
     constructor(email, password){
         this.email=email;
         this.password=password
     }
+    get email(){
+        return `${this._email}`
+    }
+
+    set email(value){
+        this._email=value
+    }
+
     get password(){
-        return this._password
+        return `${this._password}sarita`
 
     }
+
     set password(value){
-        this._password=this.value
+        this._password=value
     }
 }
 
 const sarita = new User("sarita@gmail.com","abc");
+console.log(sarita.email);
 console.log(sarita.password);
